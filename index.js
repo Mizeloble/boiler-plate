d75e230b671abe15939cc949e1c94254c18585e7
Mizeloble <Mizeloble@gmail.com>
2021-02-14 00:26:38 +0900

    mongoURI encription

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import User from "./models/User.js";
-import bodyParser from "body-parser";
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
 
 const app = express();
-const port =3000;
+const port =5000;
 
-//Decodes parsed data from the client 
-//with the type "application/x-www-form-urlencoded"
-app.use(bodyParser.urlencoded({extended: true}));
-//with the type "application/json"
-app.use(bodyParser.json());
+// Decodes parsed data from the client 
+app.use(bodyParser.urlencoded({extended: true})); // with the type "application/x-www-form-urlencoded"
+app.use(bodyParser.json()); //with the type "application/json"
 
-
-
-mongoose.connect('mongodb+srv://mizeloble:rlawlghks@testdb.edsbq.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
-
-app.get('/', (req, res) => {res.send('Hello World! Nodemon Works')});
+app.get('/', (req, res) => {res.send('<h2>Hello World! Nodemon Works</h2>')});
 
 app.post('/register', (req,res) => {
   //Get user information from the client and put that into the database
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if(err) return res.json({success : false, err});
-    return res.status(200).json({success : true});
+    return res.status(200).json({success : true});  //status(200) = 성공 
   })
 
 })
